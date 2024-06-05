e83cdb8591b0871f15b8015eabc5506c3433c9c1
Ani Dal <zbaker985@gmail.com>
2024-06-05 16:48:41 -0500

    Added data to Chiroptera.txt and implemented data extraction for that file

--- Classification/Chiroptera.java
+++ Classification/Chiroptera.java
@@ -9,13 +9,19 @@ Yingpterochiroptera
 */
 
 package Classification;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
-class Chiroptera{
+public class Chiroptera{
 	private static final String DOMAIN = "Eukaryota";
 	private static final String KINGDOM = "Animalia";
 	private static final String PHYLUM = "Chordata";
 	private static final String ANIMALCLASS = "Mammalia";
 	private static final String ORDER = "Chiroptera";
+	
+
 	
 	public Chiroptera(){
 		System.out.println("Chiroptera order established");
@@ -27,4 +33,54 @@ class Chiroptera{
 	public String getPhylum(){return PHYLUM;}
 	public String getAnimalClass(){return ANIMALCLASS;}
 	public String getOrder(){return ORDER;}
+	
+	//Retrieve data from text file
+	//Mediator function
+	public void getData(String requestedData) throws IOException{
+		//Initialize classes
+		File file = new File("C:\\Users\\zbake\\Desktop\\Programming\\GitHub\\Bat_Taxonomic_Lookup\\Classification\\Data\\Chiroptera.txt");	//Remove hard-code later
+		FileReader fr = new FileReader(file);
+		BufferedReader br = new BufferedReader(fr);
+		
+		//Get contents of file
+		String fullString = "";	//String of full file transcription
+		String line;
+		while((line = br.readLine()) != null){fullString += line;}
+		
+		//Call requested data based on
+		if(requestedData.equals("order")){displayOrderData(fullString);}
+		else if(requestedData.equals("frequency")){displayFrequencyData(fullString);}
+		else if(requestedData.equals("diversity")){displayDiversityData(fullString);}
+		
+		//Close resources
+		br.close();
+		fr.close();
+	}
+	
+	//Displays data on the order
+	private void displayOrderData(String fullString){
+		//Extract substring of needed data
+		String finalString = fullString.substring(fullString.indexOf("*Order") + 6,
+			fullString.indexOf("*Frequency"));
+		//Use data - temporary print statement
+		System.out.println(finalString);
+	}
+	
+	//Displays data on the frequency of bats
+	private void displayFrequencyData(String fullString){
+		//Extract substring of needed data
+		String finalString = fullString.substring(fullString.indexOf("*Frequency") + 10,
+			fullString.indexOf("*Diversity"));
+		//Use data - temporary print statement
+		System.out.println(finalString);
+	}
+	
+	//Displays data on overview of diversity of bats
+	private void displayDiversityData(String fullString){
+		//Extract substring of needed data
+		String finalString = fullString.substring(fullString.indexOf("*Diversity") + 10,
+			fullString.indexOf("*ENDDOC"));
+		//Use data - temporary print statement
+		System.out.println(finalString);
+	}
 }
--- Main.java
+++ Main.java
@@ -1,16 +1,23 @@
 import Classification.*;
 import GUI.*;
+import java.io.IOException;
 
 class Main{
 	//Entry point main
-	public static void main(String[] args){
-		classTest.initAll();
+	public static void main(String[] args) throws IOException{
+		Chiroptera c = new Chiroptera();
+		c.getData("order");
+		System.out.println("\n\n");
+		c.getData("frequency");
+		System.out.println("\n\n");
+		c.getData("diversity");
+		System.out.println("\n\n");
 	}
 	
 	//Initialization
 	//Calls all initialization functions
-	private static void initAll(){
-		return;
-	}
+	//private static void initAll(){
+	//	return;
+	//}
 
 }
