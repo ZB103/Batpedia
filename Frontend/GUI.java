01888bebd45ff1fa063b97325f2d1d0330b120ff
Ani Dal <zbaker985@gmail.com>
2024-07-10 12:26:38 -0500

    Beginning GUI

---
+++ Frontend/GUI.java
@@ -0,0 +1,11 @@
+package Frontend;
+import java.io.*;
+import javax.swing.*;
+
+// Main class
+public class GUI {
+ 
+    public GUI(){
+        System.out.println("a");
+    }
+}	
--- Main.java
+++ Main.java
@@ -1,39 +1,39 @@
 import Classification.*;
-import GUI.*;
+import Frontend.*;
 import java.io.IOException;
 
 class Main{
 	//Entry point main
 	public static void main(String[] args) throws IOException{
-		//Order
-		Chiroptera chiroptera = new Chiroptera();
-		Suborder yinpterochiroptera = new Suborder("Yinpterochiroptera");
-		Suborder yangochiroptera = new Suborder("Yangochiroptera");
-		//Yango
-		String ya = "Yangochiroptera";
-		Family emballonuridae = new Family("Emballonuridae", "Sac-Winged & Sheath-Tailed Bats", ya);
-		Family furipteridae = new Family("Furipteridae", "Smoky Bat & Thumbless Bat", ya);
-		Family miniopteridae = new Family("Miniopteridae", "Bent/Long-Winged Bats", ya);
-		Family molossidae = new Family("Molossidae", "Free-Tailed Bats", ya);
-		Family mormoopidae = new Family("Mormoopidae", "Mustached & Ghost-Faced & Naked-Backed Bats", ya);
-		Family mystacinidae = new Family("Mystacinidae", "New Zealand Short-Tailed Bats", ya);
-		Family myzopodidae = new Family("Myzopodidae", "Sucker-Footed Bats", ya);
-		Family natalidae = new Family("Natalidae", "Funnel-Eared Bats", ya);
-		Family noctilionidae = new Family("Noctilionidae", "Bulldog Bats", ya);
-		Family phyllostomidae = new Family("Phyllostomidae", "Leaf-Nosed Bats", ya);
-		Family thyropteridae = new Family("Thyropteridae", "Disk-Winged Bats", ya);
-		Family vespertillionidae = new Family("Vespertillionidae", "Evening Bats", ya);
-		//Yin
-		String yi = "Yinpterochiroptera";
-		Family craseonycteridae = new Family("Craseonycteridae", "Kitti's Hog-Nosed/Bumblebee Bat", yi);
-		Family hipposideridae = new Family("Hipposideridae", "Old World Leaf-Nosed Bats", yi);
-		Family megadermatidae = new Family("Megadermatidae", "False Vampire Bats", yi);
-		Family pteropodidae = new Family("Pteropodidae", "Megabats", yi);
-		Family rhinolophidae = new Family("Rhinolophidae", "Horseshoe Bats", yi);
-		Family rhinopomatidae = new Family("Rhinopomatidae", "Mouse-Tailed Bats", yi);
+		// //Order
+		// Chiroptera chiroptera = new Chiroptera();
+		// Suborder yinpterochiroptera = new Suborder("Yinpterochiroptera");
+		// Suborder yangochiroptera = new Suborder("Yangochiroptera");
+		// //Yango
+		// String ya = "Yangochiroptera";
+		// Family emballonuridae = new Family("Emballonuridae", "Sac-Winged & Sheath-Tailed Bats", ya);
+		// Family furipteridae = new Family("Furipteridae", "Smoky Bat & Thumbless Bat", ya);
+		// Family miniopteridae = new Family("Miniopteridae", "Bent/Long-Winged Bats", ya);
+		// Family molossidae = new Family("Molossidae", "Free-Tailed Bats", ya);
+		// Family mormoopidae = new Family("Mormoopidae", "Mustached & Ghost-Faced & Naked-Backed Bats", ya);
+		// Family mystacinidae = new Family("Mystacinidae", "New Zealand Short-Tailed Bats", ya);
+		// Family myzopodidae = new Family("Myzopodidae", "Sucker-Footed Bats", ya);
+		// Family natalidae = new Family("Natalidae", "Funnel-Eared Bats", ya);
+		// Family noctilionidae = new Family("Noctilionidae", "Bulldog Bats", ya);
+		// Family phyllostomidae = new Family("Phyllostomidae", "Leaf-Nosed Bats", ya);
+		// Family thyropteridae = new Family("Thyropteridae", "Disk-Winged Bats", ya);
+		// Family vespertillionidae = new Family("Vespertillionidae", "Evening Bats", ya);
+		// //Yin
+		// String yi = "Yinpterochiroptera";
+		// Family craseonycteridae = new Family("Craseonycteridae", "Kitti's Hog-Nosed/Bumblebee Bat", yi);
+		// Family hipposideridae = new Family("Hipposideridae", "Old World Leaf-Nosed Bats", yi);
+		// Family megadermatidae = new Family("Megadermatidae", "False Vampire Bats", yi);
+		// Family pteropodidae = new Family("Pteropodidae", "Megabats", yi);
+		// Family rhinolophidae = new Family("Rhinolophidae", "Horseshoe Bats", yi);
+		// Family rhinopomatidae = new Family("Rhinopomatidae", "Mouse-Tailed Bats", yi);
 		
 		
 		//testing
-		
+		new GUI();
 	}
 }
